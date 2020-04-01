d6a541ca92dfc19098402ba68cbfefa99738b60e
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-03-31 18:22:52 -0600

    Working POST

--- api/src/main/resources/db/migration/V1__add_baz_table.sql
+++ api/src/main/resources/db/migration/V1__add_baz_table.sql
@@ -1,6 +1,6 @@
 create table BAZ
 (
-    id         BIGINT PRIMARY KEY,
+    id         BIGSERIAL PRIMARY KEY,
     boo        TEXT      null,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE('UTC'::TEXT, now()),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE('UTC'::TEXT, now()),
