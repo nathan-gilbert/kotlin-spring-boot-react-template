6a5744579bea0f5a189f39e4a37617a96c79d5cb
Nathan Gilbert <nathan.gilbert@gmail.com>
2021-02-23 19:18:02 -0700

    remove time zone from database columns

--- api/src/main/resources/db/migration/V1__add_baz_table.sql
+++ api/src/main/resources/db/migration/V1__add_baz_table.sql
@@ -2,7 +2,7 @@ create table BAZ
 (
     id         BIGSERIAL PRIMARY KEY,
     msg        TEXT      null,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE('UTC'::TEXT, now()),
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE('UTC'::TEXT, now()),
+    created_at TIMESTAMP DEFAULT TIMEZONE('UTC'::TEXT, now()),
+    updated_at TIMESTAMP DEFAULT TIMEZONE('UTC'::TEXT, now()),
     deleted_at TIMESTAMP null
 );
